a64301c07ad675326281c2b1fa5a53717eba6dc0
James Tranovich <jtranovich@gmail.com>
2015-07-07 16:41:37 -0700

    Removing toggled panel.

--- demo.js
+++ demo.js
@@ -65,49 +65,6 @@ $(document).ready(function () {
     }
   }
 
-  var showPanelLabel = "Choose<br/>Background";
-  var hidePanelLabel = "Hide<br/>Panel";
-  function setChoosePanelVisibility(isVisible) {
-    var togglePanelElement = document.getElementById("togglePanelButton");
-    var spanQuery = $("span", togglePanelElement);
-
-    if (isVisible) {
-      // If choose background panel is being shown, hide toggle button
-      // border so top of button is aligned with other buttons
-      $("#choosePanel").addClass("showing");
-      spanQuery.html(hidePanelLabel);
-    } else {
-      // If choose background panel is being hidden, show toggle button
-      // border to provide contrast against picture
-      $("#choosePanel").removeClass("showing");
-      spanQuery.html(showPanelLabel);
-    }
-  }
-
-  function isChoosePanelVisible() {
-    return $("#choosePanel").hasClass("showing");
-  }
-
-  // property and function used to keep track of when the choose background control panel
-  // should be hidden after an inactivity period
-  var hidePanelTimeoutId = null;
-  function resetHidePanelTimeout() {
-    // First clear any previous timeout
-    if (hidePanelTimeoutId != null) {
-      clearTimeout(hidePanelTimeoutId);
-      hidePanelTimeoutId = null;
-    }
-
-    if (!isSensorConnected || (engagedUser == null)) {
-      // if there is no engaged user or no sensor connected, we hide the choose background
-      // control panel after 10 seconds
-      hidePanelTimeoutId = setTimeout(function () {
-        setChoosePanelVisibility(false);
-        hidePanelTimeoutId = null;
-      }, 10000);
-    }
-  }
-
   // Update sensor state and perform UI transitions (showing/hiding appropriate UI elements)
   // related to sensor status or engagement state changes
   var delayedConfigTimeoutId = null;
@@ -127,6 +84,7 @@ $(document).ready(function () {
         var immediateConfig = {};
         var delayedConfig = {};
         immediateConfig[Kinect.INTERACTION_STREAM_NAME] = { "enabled": true };
+        immediateConfig[Kinect.SKELETON_STREAM_NAME] = { "enabled": true };
         immediateConfig[Kinect.USERVIEWER_STREAM_NAME] = { "resolution": streamImageResolution };
         immediateConfig[Kinect.BACKGROUNDREMOVAL_STREAM_NAME] = { "resolution": streamImageResolution };
 
@@ -166,8 +124,6 @@ $(document).ready(function () {
 
     isSensorConnected = newIsSensorConnected;
     engagedUser = newEngagedUser;
-
-    resetHidePanelTimeout();
   }
 
   // Get the id of the engaged user, if present, or null if there is no engaged user
@@ -205,11 +161,11 @@ $(document).ready(function () {
       updateUserState(false, engagedUser, sensorToConfig);
     }
   });
-  var uiAdapter = KinectUI.createAdapter(sensor);
 
   function sketch(processing) {
     processing.draw = function() {},
     processing.setup = function() {
+      var uiAdapter = KinectUI.createAdapter(sensor);
       uiAdapter.promoteButtons();
       cursor = uiAdapter.createDefaultCursor();
       userViewerCanvasElement = document.getElementById("userViewerCanvas");
@@ -228,24 +184,9 @@ $(document).ready(function () {
           break;
         }
       });
-
-      setChoosePanelVisibility(false);
-      $("#togglePanelButton").click(function (event) {
-        if (isChoosePanelVisible()) {
-          setChoosePanelVisibility(false);
-        } else {
-          setChoosePanelVisibility(true);
-          resetHidePanelTimeout();
-        }
-      });
-      $(".imgButtonContainer .kinect-button").click(function (event) {
-        var imgElement = $("img", event.currentTarget)[0];
-        document.getElementById("backgroundImg").src = imgElement.src;
-        resetHidePanelTimeout();
-      });
     }
   }
 
   var canvas = document.getElementById("userViewerCanvas");
-  var p = new Processing(canvas, sketch);
+  var processing = new Processing(canvas, sketch);
 });
